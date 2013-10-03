64b46fac273c612c964b7c6cca835bb2930a9bd1
YeLee <maishangming@126.com>
2013-10-03 14:38:29 +0800

    更新Flash插件的下载脚本和在线视频播放脚本

--- shell/mozilla/dlflash.sh
+++ shell/mozilla/dlflash.sh
@@ -7,12 +7,9 @@ fi
 tempdir=$(mktemp -d)
 OS_BIT=32
 
-if [ $(uname -m) = "x86_64" ];
-then
-	let "OS_BIT*=2"
-fi
+[ $(uname -m) = "x86_64" ] && let "OS_BIT*=2"
 
-wget -q --user-agent="Mozilla/5.0" "http://get.adobe.com/cn/flashplayer/completion/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_$OS_BIT-bit" -O - |grep -o "http.*tar.gz" |axel - -o $tempdir/install_flash_player_11_linux.tar.gz
+wget -q --user-agent="Mozilla/5.0" "http://get.adobe.com/cn/flashplayer/download/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_${OS_BIT}-bit&standalone=1" -O - |grep -o "http.*tar.gz" |axel - -o $tempdir/install_flash_player_11_linux.tar.gz
 tar xf $tempdir/install_flash_player_11_linux.tar.gz -C $tempdir
 mkdir -p ~/.mozilla/plugins
 cp $tempdir/libflashplayer.so ~/.mozilla/plugins 

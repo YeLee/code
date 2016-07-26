4e95c42402250c38bfbe2f774d282166edbc46c4
YeLee <YeLee@users.noreply.github.com>
2016-07-26 16:57:35 +0800

    Update Flash download url

--- shell/mozilla/dlflash.sh
+++ shell/mozilla/dlflash.sh
@@ -9,7 +9,7 @@ OS_BIT=32
 
 [ $(uname -m) = "x86_64" ] && let "OS_BIT*=2"
 
-wget -q --user-agent="Mozilla/5.0" "http://get.adobe.com/cn/flashplayer/download/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_${OS_BIT}-bit&standalone=1" -O - |grep -o "http.*tar.gz" |axel - -o $tempdir/install_flash_player_11_linux.tar.gz
+wget -q --user-agent="Mozilla/5.0" "https://get.adobe.com/cn/flashplayer/download/?installer=FP_11.2_for_other_Linux_${OS_BIT}-bit_(.tar.gz)_-_NPAPI&standalone=1" -O - |grep -o "http.*tar.gz" |axel - -o $tempdir/install_flash_player_11_linux.tar.gz
 tar xf $tempdir/install_flash_player_11_linux.tar.gz -C $tempdir
 mkdir -p ~/.mozilla/plugins
 cp $tempdir/libflashplayer.so ~/.mozilla/plugins 
